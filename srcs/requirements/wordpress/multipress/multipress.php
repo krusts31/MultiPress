e86026aab3267f036c932d54c197812d7e770cce
Bad Madafucker <badmadafucker@bads-mbp.lan>
2024-01-14 22:45:45 +1300

    added phpunit test sameple unut tests pass

--- srcs/requirements/wordpress/multipress/multipress.php
+++ srcs/requirements/wordpress/multipress/multipress.php
@@ -1,14 +1,9 @@
 <?php
-/*
-Plugin Name:  WPBeginner Plugin Tutorial
-Plugin URI:   https://www.wpbeginner.com
-Description:  A short little description of the plugin. It will be displayed on the Plugins page in WordPress admin area.
-Version:      1.0
-Author:       WPBeginner
-Author URI:   https://www.wpbeginner.com
-License:      GPL2
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  wpb-tutorial
-Domain Path:  /languages
-*/
-?>
+/**
+* Plugin Name: test-plugin
+* Plugin URI: https://www.your-site.com/
+* Description: Test.
+* Version: 0.1
+* Author: your-name
+* Author URI: https://www.your-site.com/
+**/
