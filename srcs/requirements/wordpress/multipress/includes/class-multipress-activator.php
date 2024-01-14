ddfcdd62bb0feeab3bcf0949290624361030c6b9
Bad Madafucker <badmadafucker@bads-mbp.lan>
2024-01-15 11:35:45 +1300

    added plugin template

---
+++ srcs/requirements/wordpress/multipress/includes/class-multipress-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://olgrounds.dev
+ * @since      1.0.0
+ *
+ * @package    Multipress
+ * @subpackage Multipress/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Multipress
+ * @subpackage Multipress/includes
+ * @author     Aleksandrs Marks Krusts <akrusts@olgrounds.dev>
+ */
+class Multipress_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
