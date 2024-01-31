02882fd076ac442cef2f815f162bd7c3032e8f5b
Bad Madafucker <badmadafucker@bads-mbp.lan>
2024-02-01 10:59:31 +1300

    added the filter

--- chagne_url.php
+++ chagne_url.php
@@ -1,4 +1,6 @@
 <?php
+add_filter( 'wp_get_attachment_image_src', 'custom_product_image_src', 10, 4 );
+
 function custom_product_image_src( $image, $attachment_id, $size, $icon ) {
     // Check if this is a WooCommerce product image
     if ( get_post_type( $attachment_id ) === 'product' || get_post_type( get_post( $attachment_id )->post_parent ) === 'product' ) {
